f27b1198dd6fd6b477d6453bc0251e9df02b70ac
arcnmx <arcnmx@users.noreply.github.com>
2018-02-25 06:39:35 -0500

    vcp features

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -31,6 +31,68 @@ extern {
 	) -> DDCA_Status;
 	pub fn ddca_close_display(ddca_dh: DDCA_Display_Handle) -> DDCA_Status;
 
+	pub fn ddca_get_capabilities_string(
+		ddca_dh: DDCA_Display_Handle,
+		p_caps: *mut *mut c_char,
+	) -> DDCA_Status;
+
+	pub fn ddca_parse_capabilities_string(
+		capabilities_string: *mut c_char,
+		p_parsed_capabilities: *mut *mut DDCA_Capabilities,
+	) -> DDCA_Status;
+
+	pub fn ddca_free_parsed_capabilities(
+		pcaps: *mut c_char,
+	);
+
+	pub fn ddca_report_parsed_capabilities(
+		pcaps: *mut DDCA_Capabilities,
+		depth: c_int,
+	);
+
+	pub fn ddca_get_feature_info_by_vcp_version(
+		feature_code: DDCA_Vcp_Feature_Code,
+		mccs_version_id: DDCA_MCCS_Version_Id,
+		p_info: *mut *mut DDCA_Version_Feature_Info,
+	) -> DDCA_Status;
+
+	pub fn ddca_get_feature_name(feature_code: DDCA_Vcp_Feature_Code) -> *mut c_char;
+
+	pub fn ddca_get_simple_sl_value_table(
+		feature_code: DDCA_Vcp_Feature_Code,
+		mccs_version_id: DDCA_MCCS_Version_Id,
+		p_value_table: *mut DDCA_Feature_Value_Table,
+	) -> DDCA_Status;
+
+	pub fn ddca_get_simple_nc_feature_value_name(
+		ddca_dh: DDCA_Display_Handle,
+		feature_code: DDCA_Vcp_Feature_Code,
+		feature_value: u8,
+		p_feature_name: *mut *mut c_char,
+	) -> DDCA_Status;
+
+	pub fn ddca_free_feature_info(info: *mut DDCA_Version_Feature_Info) -> DDCA_Status;
+
+	pub fn ddca_get_mccs_version(
+		ddca_dh: DDCA_Display_Handle,
+		pspec: *mut DDCA_MCCS_Version_Spec,
+	) -> DDCA_Status;
+
+	pub fn ddca_get_mccs_version_id(
+		ddca_dh: DDCA_Display_Handle,
+		p_id: *mut DDCA_MCCS_Version_Id,
+	) -> DDCA_Status;
+
+	/// do not free
+	pub fn ddca_get_edid_by_display_ref(
+		ddca_dref: DDCA_Display_Ref,
+		pbytes: *mut *mut u8,
+	) -> DDCA_Status;
+
+	pub fn ddca_free_table_value_response(
+		table_value_response: *mut DDCA_Table_Value,
+	);
+
 	pub fn ddca_get_any_vcp_value(
 		ddca_dh: DDCA_Display_Handle,
 		feature_code: DDCA_Vcp_Feature_Code,
@@ -207,6 +269,121 @@ pub const DDCA_VUNK: DDCA_MCCS_Version_Id = DDCA_VNONE;
 
 pub type DDCA_Vcp_Feature_Code = u8;
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+/// an entry of {0, NULL} terminates the list
+pub struct DDCA_Feature_Value_Entry {
+	pub value_code: u8,
+	pub value_name: *mut c_char,
+}
+
+pub type DDCA_Feature_Value_Table = *mut DDCA_Feature_Value_Entry;
+
+pub type DDCA_Version_Feature_Flags = u16;
+pub const DDCA_RO: DDCA_Version_Feature_Flags = 0x0400;
+pub const DDCA_WO: DDCA_Version_Feature_Flags = 0x0200;
+pub const DDCA_RW: DDCA_Version_Feature_Flags = 0x0100;
+pub const DDCA_READABLE: DDCA_Version_Feature_Flags = DDCA_RO | DDCA_RW;
+pub const DDCA_WRITABLE: DDCA_Version_Feature_Flags = DDCA_WO | DDCA_RW;
+pub const DDCA_STD_CONT: DDCA_Version_Feature_Flags = 0x0080;
+pub const DDCA_COMPLEX_CONT: DDCA_Version_Feature_Flags = 0x0040;
+pub const DDCA_SIMPLE_NC: DDCA_Version_Feature_Flags = 0x0020;
+pub const DDCA_COMPLEX_NC: DDCA_Version_Feature_Flags = 0x0010;
+pub const DDCA_WO_NC: DDCA_Version_Feature_Flags = 0x0008;
+pub const DDCA_NORMAL_TABLE: DDCA_Version_Feature_Flags = 0x0004;
+pub const DDCA_WO_TABLE: DDCA_Version_Feature_Flags = 0x0002;
+pub const DDCA_CONT: DDCA_Version_Feature_Flags = DDCA_STD_CONT|DDCA_COMPLEX_CONT;
+pub const DDCA_NC: DDCA_Version_Feature_Flags = DDCA_SIMPLE_NC|DDCA_COMPLEX_NC|DDCA_WO_NC;
+pub const DDCA_NON_TABLE: DDCA_Version_Feature_Flags = DDCA_CONT | DDCA_NC;
+pub const DDCA_TABLE: DDCA_Version_Feature_Flags = DDCA_NORMAL_TABLE | DDCA_WO_TABLE;
+pub const DDCA_KNOWN: DDCA_Version_Feature_Flags = DDCA_CONT | DDCA_NC | DDCA_TABLE;
+pub const DDCA_DEPRECATED: DDCA_Version_Feature_Flags = 0x0001;
+
+pub type DDCA_Global_Feature_Flags = u16;
+pub const DDCA_SYNTHETIC: DDCA_Global_Feature_Flags = 0x8000;
+
+/// union (DDCA_Version_Feature_Flags, DDCA_Global_Feature_Flags)
+pub type DDCA_Feature_Flags = u16;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct DDCA_Version_Feature_Info {
+	pub marker: [c_char; 4],
+	pub feature_code: DDCA_Vcp_Feature_Code,
+	pub vspec: DDCA_MCCS_Version_Spec,
+	pub version_id: DDCA_MCCS_Version_Id,
+	pub desc: *mut c_char,
+	/// valid when DDCA_SIMPLE_NC set
+	pub sl_values: DDCA_Feature_Value_Table,
+	pub feature_name: *mut c_char,
+	pub feature_flags: DDCA_Feature_Flags,
+}
+
+impl DDCA_Version_Feature_Info {
+	pub fn sl_values_len(&self) -> usize {
+		if self.feature_flags & DDCA_SIMPLE_NC != 0 {
+			let mut ptr = self.sl_values;
+			let mut len = 0;
+			unsafe {
+				while (*ptr).value_code != 0 || !(*ptr).value_name.is_null() {
+					ptr = ptr.offset(1);
+					len += 1;
+				}
+			}
+
+			len
+		} else {
+			0
+		}
+	}
+
+	pub fn sl_values(&self) -> &[DDCA_Feature_Value_Entry] {
+		let len = self.sl_values_len();
+		if len == 0 {
+			&[]
+		} else {
+			unsafe {
+				from_raw_parts(self.sl_values as *const _, len)
+			}
+		}
+	}
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct DDCA_Cap_Vcp {
+	pub marker: [c_char; 4],
+	pub feature_code: DDCA_Vcp_Feature_Code,
+	pub value_ct: c_int,
+	pub values: *mut u8,
+}
+
+impl DDCA_Cap_Vcp {
+	pub fn values(&self) -> &[u8] {
+		unsafe {
+			from_raw_parts(self.values as *const _, self.value_ct as usize)
+		}
+	}
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct DDCA_Capabilities {
+	pub marker: [c_char; 4],
+	pub unparsed_string: *mut c_char,
+	pub version_spec: DDCA_MCCS_Version_Spec,
+	pub vcp_code_ct: c_int,
+	pub vcp_codes: *mut DDCA_Cap_Vcp,
+}
+
+impl DDCA_Capabilities {
+	pub fn vcp_codes(&self) -> &[DDCA_Cap_Vcp] {
+		unsafe {
+			from_raw_parts(self.vcp_codes as *const _, self.vcp_code_ct as usize)
+		}
+	}
+}
+
 pub type DDCA_Vcp_Value_Type = c_int;
 pub const DDCA_NON_TABLE_VCP_VALUE: DDCA_Vcp_Value_Type = 1;
 pub const DDCA_TABLE_VCP_VALUE: DDCA_Vcp_Value_Type = 2;
@@ -274,9 +451,7 @@ pub struct DDCA_Any_Vcp_Value {
 
 impl DDCA_Any_Vcp_Value {
 	pub unsafe fn c_nc(&self) -> &DDCA_Non_Table_Value {
-		unsafe {
-			::std::mem::transmute(self.t())
-		}
+		::std::mem::transmute(self.t())
 	}
 
 	pub unsafe fn t(&self) -> &_DDCA_Table_Value {
