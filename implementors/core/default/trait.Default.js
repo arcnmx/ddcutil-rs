6f6afbda3f3c09c408dc3868ac7574ed2b0227ed
ghost <ghost@konpa.ku>
2018-02-25 22:48:55 +0000

    0.0.2 - a23329ac06bfc5d95eb92a8353106e8db8f22a79

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ddcutil"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"ddcutil/enum.OutputLevel.html\" title=\"enum ddcutil::OutputLevel\">OutputLevel</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
