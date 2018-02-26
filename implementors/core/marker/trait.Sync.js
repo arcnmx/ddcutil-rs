037b2474ab0b503a13263b565b00ba15de956a79
ghost <ghost@konpa.ku>
2018-02-26 03:24:10 +0000

    0.0.3 - 3a631cdc92fcffcfc6e97fa29b4ed607fafc52d8

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ddcutil"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"ddcutil/struct.DisplayInfo.html\" title=\"struct ddcutil::DisplayInfo\">DisplayInfo</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"ddcutil/struct.DisplayInfoList.html\" title=\"struct ddcutil::DisplayInfoList\">DisplayInfoList</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
